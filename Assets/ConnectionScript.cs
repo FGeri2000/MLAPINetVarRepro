c8860f5bf071e14f88571cee15cfa4e7bc03533c
FGeri2000 <46203432+FGeri2000@users.noreply.github.com>
2020-05-24 21:59:11 +0200

    Initial commit

---
+++ Assets/ConnectionScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using MLAPI;
+using UnityEngine.UI;
+
+public class ConnectionScript : MonoBehaviour
+{
+    public void StartServer() { NetworkingManager.Singleton.StartServer(); }
+    public void Connect() {
+        GetComponent<LiteNetLibTransport.LiteNetLibTransport>().Address = GameObject.Find("InputField").GetComponent<InputField>().text;
+        NetworkingManager.Singleton.StartClient(); }
+    public void Quit() { Application.Quit(); }
+}
---
+++ Assets/ReproScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using MLAPI;
+using MLAPI.NetworkedVar;
+
+public class ReproScript : NetworkedBehaviour
+{
+    //Using generic to illustrate problem scope
+    NetworkedVar<Vector2> input;
+
+    private void Start()
+    {
+        input = new NetworkedVar<Vector2>(Vector2.zero);
+    }
+    private void Update()
+    {
+        //Assign value to the NetworkedVar on the client
+        if (IsOwner) input.Value = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //Display information about the NetworkedVar
+        GameObject.Find("Display").GetComponent<UnityEngine.UI.Text>().text = 
+            string.Format("owner: {0}{4}value: {1}{4}dirty: {2}{4}last synced time: {3}",
+            IsOwner,
+            input.Value,
+            input.isDirty,
+            input.LastSyncedTime,
+            System.Environment.NewLine);
+    }
+}
